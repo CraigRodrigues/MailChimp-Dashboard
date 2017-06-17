b56e76b29aacdad1a43a3021bc78b26cacb1ce85
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-17 19:35:53 -0400

    Add tests for V3 API call for account details

--- server/mailchimp/account.test.js
+++ server/mailchimp/account.test.js
@@ -1,6 +1,36 @@
 const mailchimp = require('./account')
 
 test('It should receive an object', () => {
-  let actual = mailchimp.accountDetails()
-  expect(typeof actual).toBe('object')
+  return mailchimp.accountDetails()
+    .then(data => {
+      expect(typeof data).toBe('object')
+    })
+})
+
+test('It should have seven properties', () => {
+  return mailchimp.accountDetails()
+    .then(data => {
+      expect(Object.keys(data)).toHaveLength(7)
+    })
+})
+
+test('It should contain all the properties needed', () => {
+  let expected = [
+    'first_name',
+    'last_name',
+    'avatar_url',
+    'total_subscribers',
+    'account_industry',
+    'industry_stats',
+    'member_since'
+  ]
+
+  return mailchimp.accountDetails()
+    .then(data => {
+      let actual = Object.keys(data)
+
+      expected.forEach(prop => {
+        expect(actual).toContain(prop)
+      })
+    })
 })
