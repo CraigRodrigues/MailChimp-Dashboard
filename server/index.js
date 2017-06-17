b29fb862299246d56ff3a1edc39cd0acaa6463f5
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-17 15:56:14 -0400

    Comment out unused router for now

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
 require('dotenv').config()
 
 const express = require('express')
-const port = process.env.PORT
+// const port = process.env.PORT
+const port = 1337
 const path = require('path')
 const bodyParser = require('body-parser')
-const router = require('./router')
+// const router = require('./router')
 const app = express()
 
 app.listen(port, () => {
-  console.log(`Listening on port: ${port} in ${process.env.NODE_ENV} environment`)
+  console.log(`Listening on port: ${port}`)
 })
 
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../public/')))
 
-app.use('/api', router)
+// app.use('/api', router)
