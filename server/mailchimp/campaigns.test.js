6dd5ce4da902b8ecbbd8e087327abd0ae10e0a66
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-18 08:57:00 -0400

    (feat) Add campaign unit tests

---
+++ server/mailchimp/campaigns.test.js
@@ -0,0 +1,29 @@
+const mailchimp = require('./campaigns')
+
+test('It should receive an object', () => {
+  return mailchimp.campaignReports()
+    .then(data => {
+      expect(typeof data).toBe('object')
+    })
+})
+
+test('It should have only one property', () => {
+  return mailchimp.campaignReports()
+    .then(data => {
+      expect(Object.keys(data)).toHaveLength(1)
+    })
+})
+
+test('It should have a property called "campaigns"', () => {
+  return mailchimp.campaignReports()
+    .then(data => {
+      expect(data.hasOwnProperty('campaigns')).toBe(true)
+    })
+})
+
+test('Campaigns property should have a length of 34', () => {
+  return mailchimp.campaignReports()
+    .then(data => {
+      expect(data.campaigns).toHaveLength(34)
+    })
+})
