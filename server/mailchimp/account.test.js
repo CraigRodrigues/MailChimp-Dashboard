908a92c1bbbdb23f26f6ff8c4cdfb80f78ee1452
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-18 09:12:32 -0400

    (feat) Add describe blocks

--- server/mailchimp/account.test.js
+++ server/mailchimp/account.test.js
@@ -1,36 +1,40 @@
 const mailchimp = require('./account')
 
-test('It should receive an object', () => {
-  return mailchimp.accountDetails()
-    .then(data => {
-      expect(typeof data).toBe('object')
-    })
-})
+describe('Getting Account Details', () => {
 
-test('It should have seven properties', () => {
-  return mailchimp.accountDetails()
-    .then(data => {
-      expect(Object.keys(data)).toHaveLength(7)
-    })
-})
-
-test('It should contain all the properties needed', () => {
-  let expected = [
-    'first_name',
-    'last_name',
-    'avatar_url',
-    'total_subscribers',
-    'account_industry',
-    'industry_stats',
-    'member_since'
-  ]
-
-  return mailchimp.accountDetails()
-    .then(data => {
-      let actual = Object.keys(data)
-
-      expected.forEach(prop => {
-        expect(actual).toContain(prop)
+  test('It should receive an object', () => {
+    return mailchimp.accountDetails()
+      .then(data => {
+        expect(typeof data).toBe('object')
       })
-    })
+  })
+
+  test('It should have seven properties', () => {
+    return mailchimp.accountDetails()
+      .then(data => {
+        expect(Object.keys(data)).toHaveLength(7)
+      })
+  })
+
+  test('It should contain all the properties needed', () => {
+    let expected = [
+      'first_name',
+      'last_name',
+      'avatar_url',
+      'total_subscribers',
+      'account_industry',
+      'industry_stats',
+      'member_since'
+    ]
+
+    return mailchimp.accountDetails()
+      .then(data => {
+        let actual = Object.keys(data)
+
+        expected.forEach(prop => {
+          expect(actual).toContain(prop)
+        })
+      })
+  })
+
 })
--- server/mailchimp/campaigns.test.js
+++ server/mailchimp/campaigns.test.js
@@ -1,29 +1,33 @@
 const mailchimp = require('./campaigns')
 
-test('It should receive an object', () => {
-  return mailchimp.campaignReports()
-    .then(data => {
-      expect(typeof data).toBe('object')
-    })
-})
+describe('Getting Data From All Campaigns', () => {
 
-test('It should have only one property', () => {
-  return mailchimp.campaignReports()
-    .then(data => {
-      expect(Object.keys(data)).toHaveLength(1)
-    })
-})
+  test('It should receive an object', () => {
+    return mailchimp.campaignReports()
+      .then(data => {
+        expect(typeof data).toBe('object')
+      })
+  })
 
-test('It should have a property called "campaigns"', () => {
-  return mailchimp.campaignReports()
-    .then(data => {
-      expect(data.hasOwnProperty('campaigns')).toBe(true)
-    })
-})
+  test('It should have only one property', () => {
+    return mailchimp.campaignReports()
+      .then(data => {
+        expect(Object.keys(data)).toHaveLength(1)
+      })
+  })
 
-test('Campaigns property should have a length of 34', () => {
-  return mailchimp.campaignReports()
-    .then(data => {
-      expect(data.campaigns).toHaveLength(34)
-    })
+  test('It should have a property called "campaigns"', () => {
+    return mailchimp.campaignReports()
+      .then(data => {
+        expect(data.hasOwnProperty('campaigns')).toBe(true)
+      })
+  })
+
+  test('Campaigns property should have a length of 34', () => {
+    return mailchimp.campaignReports()
+      .then(data => {
+        expect(data.campaigns).toHaveLength(34)
+      })
+  })
+
 })
