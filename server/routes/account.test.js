ec7798efbb88e5ab0f60db9b42dcd434943bf9c9
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-18 11:58:45 -0400

    Add test for data to /api/account

--- server/routes/account.test.js
+++ server/routes/account.test.js
@@ -24,13 +24,25 @@ describe('Route Tests', () => {
         expect(response.statusCode).toBe(200)
       })
   })
-  test.skip('It should return 201 to POST /api/lists', () => {
-    // TODO
+  test('It should return 201 to POST /api/lists', () => {
+    return request(app).post('/api/lists')
+      .then(response => {
+        expect(response.statusCode).toBe(201)
+      })
   })
   test('It should return 200 to GET /api/campaigns', () => {
     return request(app).get('/api/campaigns')
       .then(response => {
         expect(response.statusCode).toBe(200)
+      })
+  })
+})
+
+describe('Account /api/account', () => {
+  test('It should return account data', () => {
+    return request(app).get('/api/account')
+      .then(data => {
+        expect(typeof data).toBe('object')
       })
   })
 })
