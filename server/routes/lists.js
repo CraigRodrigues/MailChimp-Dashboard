5fc358d464e4b06366374374929d3370ce2ec31a
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-18 14:18:56 -0400

    Complete getListData

--- server/routes/lists.js
+++ server/routes/lists.js
@@ -1,5 +1,13 @@
+const errorHandler = require('./error')
+const Mailchimp = require('../mailchimp/lists')
+
 const getListData = (req, res) => {
-  res.sendStatus(200)
+  // Batch promise.all
+  Promise.all([Mailchimp.listStats(), Mailchimp.clientData(), Mailchimp.locationData()])
+    .then(results => {
+      res.send(results)
+    })
+    .catch(error => errorHandler(error, res, 500))
 }
 
 const subscribeToList = (req, res) => {
