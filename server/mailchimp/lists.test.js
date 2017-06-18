8cb06fc94ec3d6ab9944415b1bbe6153bcfa3aac
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-18 09:24:07 -0400

    (feat) Add tests for listStats function

--- server/mailchimp/lists.test.js
+++ server/mailchimp/lists.test.js
@@ -1,6 +1,31 @@
 const mailchimp = require('./lists')
 
-describe('Basic List Metrics', () => {
+describe('Basic List Stats', () => {
+
+  test('It should return an object', () => {
+    return mailchimp.listStats()
+      .then(data => {
+        expect(typeof data).toBe('object')
+      })
+  })
+  test('It should have only one property', () => {
+    return mailchimp.listStats()
+      .then(data => {
+        expect(Object.keys(data)).toHaveLength(1)
+      })
+  })
+  test('It should have a property called "stats"', () => {
+    return mailchimp.listStats()
+      .then(data => {
+        expect(data.hasOwnProperty('stats')).toBe(true)
+      })
+  })
+  test('Stats object should have 16 properties', () => {
+    return mailchimp.listStats()
+      .then(data => {
+        expect(Object.keys(data.stats).toHaveLength(16))
+      })
+  })
 
 })
 
