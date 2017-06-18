1cf7a9c424376f7564caec17f48a899e57513d64
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-18 11:26:13 -0400

    Abstract listen server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,11 @@
-require('dotenv').config({ path: '../.env' })
-
-const PORT = process.env.PORT
-const HOST = process.env.HOST
-const ENV = process.env.NODE_ENV
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const router = require('./router')
 const app = express()
 
-app.listen(PORT, () => {
-  console.log(`Listening at ${HOST} on port ${PORT} in ${ENV} environment`)
-})
-
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../public/')))
-
 app.use('/api', router)
+
+module.exports = app
