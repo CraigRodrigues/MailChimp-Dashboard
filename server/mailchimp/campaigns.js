6540e1011dfbe9afd743c887709f5e1b7d0700e5
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-18 08:57:19 -0400

    (feat) Add campaigns http request functionality

---
+++ server/mailchimp/campaigns.js
@@ -0,0 +1,16 @@
+const axios = require('../wrapper/axios').wrapper
+
+const config = {
+  params: {
+    count: 50,
+    fields: 'campaigns'
+  }
+}
+
+const campaignReports = () => {
+  return axios.get('/campaigns', config)
+    .then(response => response.data)
+    .catch(err => console.log(err))
+}
+
+module.exports = { campaignReports }
