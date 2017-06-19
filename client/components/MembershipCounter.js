1a9b3a7fed1377be27766b7b9a90551809ce09ff
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-19 11:17:45 -0400

    Add stubbed counter component

---
+++ client/components/MembershipCounter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from 'grommet/components/Heading'
+
+export default () => (
+  <Heading strong
+    uppercase
+    truncate={false}
+    align='center'
+    tag='h5'>
+    6 Years : 4 Months : 3 Weeks : 2 Days : 10 Hours : 2 Minutes : 30 Seconds
+  </Heading>
+)
