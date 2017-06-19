d46fe7e9ebab8ea58d9846b2062ef31cc285ab49
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-19 11:17:33 -0400

    Add MemberSince component

---
+++ client/components/MemberSince.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Box from 'grommet/components/Box'
+import Headline from 'grommet/components/Headline'
+import MembershipCounter from './MembershipCounter'
+
+export default () => (
+  <Box direction='column' align='center' alignContent='center' textAlign='center' full='horizontal' pad='small'>
+    <Headline size='small' strong>
+      MailChimp User Since: January, 11, 2011
+    </Headline>
+    <MembershipCounter />
+  </Box>
+)
