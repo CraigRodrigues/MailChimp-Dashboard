47c0121665211e97bf791e6cd4711dd6e1477ff2
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-19 10:12:31 -0400

    Refactor index

--- client/index.js
+++ client/index.js
@@ -1,14 +1,9 @@
 import 'grommet/scss/vanilla/index.scss'
+
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
 
-const app = (
-  <div>
-    <App />
-  </div>
-)
-
 const target = document.getElementById('app')
 
-render(app, target)
+render(<App />, target)
