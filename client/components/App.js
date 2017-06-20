2b0815af2300348293ada0648cb95665746d6fcd
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-20 10:23:01 -0400

    Add API calls for all data and setState

--- client/components/App.js
+++ client/components/App.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 import GrommetApp from 'grommet/components/App'
 import Nav from './Nav'
 import Homepage from './Homepage'
 import Footer from './Footer'
 
+const getAccountData = () => {
+  return axios.get('/api/account')
+}
+
+const getListData = () => {
+  return axios.get('/api/lists')
+}
+
+const getCampaignData = () => {
+  return axios.get('/api/campaigns')
+}
+
+const getAllData = () => {
+  return Promise.all([getAccountData(), getListData(), getCampaignData()])
+}
+
 export default class App extends Component {
   constructor () {
     super()
-    this.state = {}
+    this.state = {
+      account: null,
+      campaign: null,
+      clients: null,
+      listStats: null,
+      locations: null
+    }
   }
 
   componentWillMount () {
     // Grab info from API
-    // Set state with it
+    getAllData().then(response => {
+      let [account, list, campaign] = response
+
+      this.setState({
+        account: account.data,
+        campaign: campaign.data.campaigns,
+        clients: list.data[1].clients,
+        listStats: list.data[0].stats,
+        locations: list.data[2].locations
+      })
+
+      console.log(this.state)
+    })
   }
 
   render () {
@@ -21,7 +55,12 @@ export default class App extends Component {
       <div>
         <GrommetApp>
           <Nav />
-          <Homepage />
+          <Homepage account={this.state.account}
+            campaign={this.state.campaign}
+            clients={this.state.clients}
+            listStats={this.state.listStats}
+            locations={this.state.locations}
+          />
           <Footer />
         </GrommetApp>
       </div>
