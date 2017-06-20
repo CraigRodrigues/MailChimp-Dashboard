7e49f8abe15dc06d042eb2fd03d1b37d6e2d1c46
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-20 09:48:56 -0400

    Convert App to class component

--- client/components/App.js
+++ client/components/App.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
+// import axios from 'axios'
 import GrommetApp from 'grommet/components/App'
 import Nav from './Nav'
 import Homepage from './Homepage'
 import Footer from './Footer'
 
-export default () => (
-  <div>
-    <GrommetApp>
-      <Nav />
-      <Homepage />
-      <Footer />
-    </GrommetApp>
-  </div>
-)
+export default class App extends Component {
+  constructor () {
+    super()
+    this.state = {}
+  }
+
+  componentWillMount () {
+    // Grab info from API
+    // Set state with it
+  }
+
+  render () {
+    return (
+      <div>
+        <GrommetApp>
+          <Nav />
+          <Homepage />
+          <Footer />
+        </GrommetApp>
+      </div>
+    )
+  }
+}
