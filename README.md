0bedb4e44e6b20dc0337127ff39144e9f6f04d1c
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-20 12:46:00 -0400

    Update ReadME and add props

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# MailChimp-Dashboard
+# MailChimp Dashboard
 
-TODO
+- A dashboard visualizing all of my personal Mailchimp account and list data for my business GG Button.
+
+- Using Mailchimp v3 API, React, Node, Express, Grommet. TDD via Jest and Supertest.
--- client/components/MemberSince.js
+++ client/components/MemberSince.js
@@ -5,7 +5,7 @@ import Headline from 'grommet/components/Headline'
 import MembershipCounter from './MembershipCounter'
 
 export default (props) => {
-  let memberSince = utils(props.memberSince)
+  let memberSince = utils.convertDate(props.memberSince)
 
   return (
     <Box direction='column'
@@ -18,7 +18,7 @@ export default (props) => {
       <Headline size='small' strong>
         MailChimp User Since {memberSince}
       </Headline>
-      <MembershipCounter />
+      <MembershipCounter time={props.memberSince} />
     </Box>
   )
 }
