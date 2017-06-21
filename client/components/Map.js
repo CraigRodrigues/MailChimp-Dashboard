b48e19d644cff61e13e29d693625c051f5473034
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-21 09:47:26 -0400

    Add colors object, number of countries per continent, number of subscribers per continent

--- client/components/Map.js
+++ client/components/Map.js
@@ -5,6 +5,37 @@ import Legend from 'grommet/components/Legend'
 import WorldMap from 'grommet/components/WorldMap'
 import Box from 'grommet/components/Box'
 
+const colors = [
+  {
+    threshold: 50,
+    colorIndex: 'graph-1'
+  },
+  {
+    threshold: 249,
+    colorIndex: 'graph-2'
+  },
+  {
+    threshold: 999,
+    colorIndex: 'accent-1'
+  },
+  {
+    threshold: 1999,
+    colorIndex: 'accent-2'
+  },
+  {
+    threshold: 2000,
+    colorIndex: 'brand'
+  }
+]
+
+const findColor = (value) => {
+  for (var i = colors.length - 1; i >= 0; i--) {
+    if (value >= colors[i].threshold) {
+      return colors[i].colorIndex
+    }
+  }
+}
+
 const mapSeries = [
   {
     continent: 'NorthAmerica',
@@ -77,12 +108,38 @@ const filterCurrentCountriesByContinent = (countries) => {
   return results
 }
 
+const numberOfCountriesPerContinent = (list) => {
+  let results = {}
+
+  for (let continent in list) {
+    results[continent] = list[continent].length
+  }
+
+  return results
+}
+
+const numberOfSubscribersPerContinent = (list) => {
+  let results = {}
+
+  for (let continent in list) {
+    results[continent] = list[continent].map(item => item.subscribers).reduce((a, b) => a + b, 0)
+  }
+
+  return results
+}
+
 export default ({ countries }) => {
   let allCountries = convertAllCountries(countries)
   let continents = filterCurrentCountriesByContinent(allCountries)
 
   console.log(allCountries)
   console.log(continents)
+
+  const numberOfCountries = numberOfCountriesPerContinent(continents)
+  const numberOfSubscribers = numberOfSubscribersPerContinent(continents)
+
+  console.log(numberOfCountries)
+  console.log(numberOfSubscribers)
 
   return (
     <Box>
@@ -95,19 +152,23 @@ export default ({ countries }) => {
       <Box align='center' pad={{vertical: 'medium'}}>
         <Legend series={[
           {
-            'label': '0-5',
+            'label': '0-50',
             'colorIndex': 'graph-1'
           },
           {
-            'label': '6-10',
+            'label': '51-249',
             'colorIndex': 'graph-2'
           },
           {
-            'label': '11-25',
+            'label': '250-999',
             'colorIndex': 'accent-1'
           },
           {
-            'label': '26+',
+            'label': '1000-1999',
+            'colorIndex': 'accent-2'
+          },
+          {
+            'label': '2000+',
             'colorIndex': 'brand'
           }
         ]} />
