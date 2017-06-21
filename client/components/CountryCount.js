e5a817720934fe63a0d53361c151606ff9875f18
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-21 08:21:19 -0400

    Update total country count

--- client/components/CountryCount.js
+++ client/components/CountryCount.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { convertToPercentage } from '../utils'
 import Box from 'grommet/components/Box'
 import Heading from 'grommet/components/Heading'
 import Value from 'grommet/components/Value'
 import GlobeIcon from 'grommet/components/icons/base/Globe'
+
+const TOTAL_COUNTRIES = 196
 
 export default ({countries}) => (
   <Box direction='column'
@@ -16,6 +19,7 @@ export default ({countries}) => (
       Countries
     </Heading>
     <Value value={countries.length}
+      label={`${convertToPercentage(countries.length / TOTAL_COUNTRIES)}% of the World`}
       icon={
         <GlobeIcon size='large'
           colorIndex='grey-1-a'
