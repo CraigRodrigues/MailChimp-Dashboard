8aacf8ecba38590e1e581034c45a940d4063c253
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-20 20:27:20 -0400

    Remove outside data obj

--- client/components/ClientsDoughnutChart.js
+++ client/components/ClientsDoughnutChart.js
@@ -1,27 +1,6 @@
 import React from 'react'
 import { Doughnut } from 'react-chartjs-2'
 
-const data = {
-  labels: [
-    'Red',
-    'Green',
-    'Yellow'
-  ],
-  datasets: [{
-    data: [300, 50, 100],
-    backgroundColor: [
-      '#FF6384',
-      '#36A2EB',
-      '#FFCE56'
-    ],
-    hoverBackgroundColor: [
-      '#FF6384',
-      '#36A2EB',
-      '#FFCE56'
-    ]
-  }]
-}
-
 const parseData = (list) => {
   return {
     labels: list.map(item => item.client),
