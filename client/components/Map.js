58a2809dc54ade5e9790b993c8b8b56ced7e3d5c
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-21 17:56:41 -0400

    Add modal layer tester

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { countriesByContinent } from '../data'
 import Heading from 'grommet/components/Heading'
+import Layer from 'grommet/components/Layer'
 import Legend from 'grommet/components/Legend'
 import WorldMap from 'grommet/components/WorldMap'
 import Box from 'grommet/components/Box'
@@ -34,53 +35,6 @@ const findColor = (value) => {
       return colors[i].colorIndex
     }
   }
-}
-
-const createMapSeries = (subscribers) => {
-  return [
-    {
-      continent: 'NorthAmerica',
-      label: 'North America',
-      value: subscribers.NorthAmerica,
-      colorIndex: findColor(subscribers.NorthAmerica),
-      onClick: () => {}
-    },
-    {
-      continent: 'SouthAmerica',
-      label: 'South America',
-      value: subscribers.SouthAmerica,
-      colorIndex: findColor(subscribers.SouthAmerica),
-      onClick: () => {}
-    },
-    {
-      continent: 'Europe',
-      label: 'Europe',
-      value: subscribers.Europe,
-      colorIndex: findColor(subscribers.Europe),
-      onClick: () => {}
-    },
-    {
-      continent: 'Africa',
-      label: 'Africa',
-      value: subscribers.Africa,
-      colorIndex: findColor(subscribers.Africa),
-      onClick: () => {}
-    },
-    {
-      continent: 'Asia',
-      label: 'Asia',
-      value: subscribers.Asia,
-      colorIndex: findColor(subscribers.Asia),
-      onClick: () => {}
-    },
-    {
-      continent: 'Australia',
-      label: 'Australia',
-      value: subscribers.Oceania,
-      colorIndex: findColor(subscribers.Oceania),
-      onClick: () => {}
-    }
-  ]
 }
 
 const convertAllCountries = (countries) => {
@@ -130,45 +84,127 @@ const numberOfSubscribersPerContinent = (list) => {
   return results
 }
 
-export default ({ countries }) => {
-  let allCountries = convertAllCountries(countries)
-  let continents = filterCurrentCountriesByContinent(allCountries)
+export default class Map extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      active: undefined,
+      continentSelected: undefined
+    }
 
-  const numberOfCountries = numberOfCountriesPerContinent(continents)
-  const numberOfSubscribers = numberOfSubscribersPerContinent(continents)
+    this._activate = this._activate.bind(this)
+    this._onClose = this._onClose.bind(this)
+  }
 
-  return (
-    <Box>
-      <Heading align='center' tag='h2'>
-        Subscriber World Map
-      </Heading>
-      <Box className='infographic-map'>
-        <WorldMap series={createMapSeries(numberOfSubscribers)} />
+  _activate () {
+    this.setState({ active: true })
+  }
+
+  _onClose () {
+    this.setState({ active: false })
+  }
+
+  createMapSeries (subscribers) {
+    return [
+      {
+        continent: 'NorthAmerica',
+        label: 'North America',
+        value: subscribers.NorthAmerica,
+        colorIndex: findColor(subscribers.NorthAmerica),
+        onClick: () => { this._activate() }
+      },
+      {
+        continent: 'SouthAmerica',
+        label: 'South America',
+        value: subscribers.SouthAmerica,
+        colorIndex: findColor(subscribers.SouthAmerica),
+        onClick: () => { this._activate() }
+      },
+      {
+        continent: 'Europe',
+        label: 'Europe',
+        value: subscribers.Europe,
+        colorIndex: findColor(subscribers.Europe),
+        onClick: () => {}
+      },
+      {
+        continent: 'Africa',
+        label: 'Africa',
+        value: subscribers.Africa,
+        colorIndex: findColor(subscribers.Africa),
+        onClick: () => {}
+      },
+      {
+        continent: 'Asia',
+        label: 'Asia',
+        value: subscribers.Asia,
+        colorIndex: findColor(subscribers.Asia),
+        onClick: () => {}
+      },
+      {
+        continent: 'Australia',
+        label: 'Australia',
+        value: subscribers.Oceania,
+        colorIndex: findColor(subscribers.Oceania),
+        onClick: () => {}
+      }
+    ]
+  }
+
+  render () {
+    let { countries } = this.props
+    let allCountries = convertAllCountries(countries)
+    let continents = filterCurrentCountriesByContinent(allCountries)
+
+    const numberOfCountries = numberOfCountriesPerContinent(continents)
+    const numberOfSubscribers = numberOfSubscribersPerContinent(continents)
+
+    let activeLayer = null
+
+    if (this.state.active) {
+      activeLayer = (
+        <Layer onClose={this._onClose}
+          closer={true}
+          align='center'>
+          <Box>Test</Box>
+        </Layer>
+      )
+    }
+
+    return (
+      <Box>
+        { activeLayer }
+        <Heading align='center' tag='h2'>
+          Subscriber World Map
+        </Heading>
+        <Box className='infographic-map'>
+          <WorldMap series={this.createMapSeries(numberOfSubscribers)} />
+        </Box>
+        <Box align='center' pad={{vertical: 'small'}}>
+          <Legend series={[
+            {
+              'label': '0-50',
+              'colorIndex': 'graph-1'
+            },
+            {
+              'label': '51-249',
+              'colorIndex': 'graph-2'
+            },
+            {
+              'label': '250-999',
+              'colorIndex': 'accent-1'
+            },
+            {
+              'label': '1000-1999',
+              'colorIndex': 'accent-2'
+            },
+            {
+              'label': '2000+',
+              'colorIndex': 'brand'
+            }
+          ]} />
+        </Box>
       </Box>
-      <Box align='center' pad={{vertical: 'small'}}>
-        <Legend series={[
-          {
-            'label': '0-50',
-            'colorIndex': 'graph-1'
-          },
-          {
-            'label': '51-249',
-            'colorIndex': 'graph-2'
-          },
-          {
-            'label': '250-999',
-            'colorIndex': 'accent-1'
-          },
-          {
-            'label': '1000-1999',
-            'colorIndex': 'accent-2'
-          },
-          {
-            'label': '2000+',
-            'colorIndex': 'brand'
-          }
-        ]} />
-      </Box>
-    </Box>
-  )
+    )
+  }
 }
