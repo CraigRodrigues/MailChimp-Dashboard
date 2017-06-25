caf383348439b1ae577b1524e7b0439f0bebcc25
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-25 14:07:08 -0400

    Covert clients to percentages

--- client/components/ClientsDoughnutChart.js
+++ client/components/ClientsDoughnutChart.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import { Doughnut } from 'react-chartjs-2'
 
+const colors = [
+  '#FF8A65', '#66BB6A', '#90CAF9', '#78909C', '#EC407A', '#BCAAA4', '#CE93D8', '#EF5350', '#FFF176', '#212121'
+]
+
 const parseData = (list) => {
+  let total = list.reduce((x, y) => x + y.members, 0)
+
   return {
     labels: list.map(item => item.client),
     datasets: [{
-      data: list.map(item => item.members),
-      backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-      ],
-      hoverBackgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-      ]
+      data: list.map(item => ((item.members / total) * 100).toFixed(2)),
+      backgroundColor: colors.splice(0, list.length),
+      hoverBackgroundColor: colors.splice(0, list.length)
     }]
   }
 }
