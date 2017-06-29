fcb02e4130f5b92e219523a190ec75ca72db617f
Craig Rodrigues <PagingCraig@gmail.com>
2017-06-28 21:31:44 -0400

    Add jsonwebtoken

--- server/router.js
+++ server/router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+// const jwt = require('express-jwt')
 const Account = require('./routes/account')
 const Lists = require('./routes/lists')
 const Campaigns = require('./routes/campaigns')
