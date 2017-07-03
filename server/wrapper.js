2248e390c310706864db490a283cb974cff632b2
Craig Rodrigues <PagingCraig@gmail.com>
2017-07-03 11:52:45 -0400

    Remove console log

--- server/wrapper.js
+++ server/wrapper.js
@@ -2,7 +2,6 @@
 const axios = require('axios')
 
 module.exports = (key = process.env.MAILCHIMP_API_KEY) => {
-  console.log(key)
   const url = `https://${key.slice(-3)}.api.mailchimp.com/3.0`
 
   return axios.create({
