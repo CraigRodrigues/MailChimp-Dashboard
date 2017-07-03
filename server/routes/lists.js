bcb34106361a7eb78ba08132fe0355ddcd9299f6
Craig Rodrigues <PagingCraig@gmail.com>
2017-07-03 15:28:22 -0400

    Remove console logs

--- server/routes/lists.js
+++ server/routes/lists.js
@@ -7,23 +7,23 @@ const getListData = (req, res) => {
       if (total_items === 1) {
         return Promise.resolve(lists[0].id)
       } else if (total_items === 0) {
-        // Throw some kind of error that there are no lists?
+        // Throw some kind of error that there are no lists
         return Promise.reject(new Error('No lists found!'))
       } else {
         // Return list with largest subscriber count
-        return Promise.resolve(lists.map(list => [list.id, list.stats.member_count])
-          .reduce((prev, curr) => {
-            if (curr[1] > prev[1]) {
-              return curr
-            } else {
-              return prev
-            }
-          })[0])
+        return Promise.resolve(
+          lists.map(list => [list.id, list.stats.member_count])
+            .reduce((prev, curr) => {
+              if (curr[1] > prev[1]) {
+                return curr
+              } else {
+                return prev
+              }
+            })[0])
       }
     })
     .then((list) => {
       // Batch async promises
-      console.log(list)
       return Promise.all([Mailchimp.listStats(req.token, list), Mailchimp.clientData(req.token, list), Mailchimp.locationData(req.token, list)])
     })
     .then(results => {
